04d4d84e9a90256daee8e094c7f531dc80615725
Mikhail Ivchenko <ematirov@gmail.com>
2017-10-10 21:33:35 +0400

    Correctly generate client

--- Sybon.Auth.Client/generate.py
+++ Sybon.Auth.Client/generate.py
@@ -1,5 +1,6 @@
-import subprocess, time, signal
-p = subprocess.Popen(["dotnet", "run", "--project", "Sybon.Auth"])
+import subprocess, time, signal, os, sys
+projectName = 'Sybon.Auth'
+p = subprocess.Popen(["dotnet", "run"], cwd = os.path.join(os.getcwd(), projectName))
 time.sleep(5)
-subprocess.call(["java", "-jar", ".\\Sybon.Auth.Client\\swagger-codegen-cli-3.0.0-20170904.171256-3.jar", "generate", "-l", "csharp", "--additional-properties", "targetFramework=v5.0,netCoreProjectFile=true,packageName=Sybon.Auth.Client", "-i", "http://localhost:5000/swagger/v1/swagger.json", "-o", "Sybon.Auth.Client\\sybon.auth.client"])
+subprocess.call(["java", "-jar", ".\\" + projectName + ".Client\\swagger-codegen-cli-3.0.0-20170904.171256-3.jar", "generate", "-l", "csharp", "--additional-properties", "targetFramework=v5.0,netCoreProjectFile=true,packageName="+projectName+".Client", "-i", "http://localhost:5000/swagger/v1/swagger.json", "-o", projectName+".Client\\"+projectName+".Client"])
 p.send_signal(signal.CTRL_C_EVENT)
