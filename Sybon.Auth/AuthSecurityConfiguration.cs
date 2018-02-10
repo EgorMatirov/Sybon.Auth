144136db76e16edbd89fefe9cd5da35939b35394
Mikhail Ivchenko <ematirov@gmail.com>
2018-02-10 11:15:01 +0400

    Add metrics. Update config usage to match Sybon.Archive and Sybon.Checking

---
+++ Sybon.Auth/AuthSecurityConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Sybon.Auth
+{
+    public class AuthSecurityConfiguration
+    {
+        public class SybonArchiveConfiguration
+        {
+            public SybonArchiveConfiguration(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+
+            private IConfiguration Configuration { get; }
+            
+            public string Url => Configuration.GetValue<string>("Url");
+        }
+
+        public class InfluxDbConfiguration
+        {
+            public InfluxDbConfiguration(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+        
+            private IConfiguration Configuration { get; }
+            
+            public string Url => Configuration.GetValue<string>("Url");
+            public string Password => Configuration.GetValue<string>("Password");
+            public string UserName => Configuration.GetValue<string>("UserName");
+            public string Database => Configuration.GetValue<string>("Database");
+        }
+        
+        public AuthSecurityConfiguration(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public SybonArchiveConfiguration SybonArchive => new SybonArchiveConfiguration(Configuration.GetSection("SybonArchive"));
+        public InfluxDbConfiguration InfluxDb => new InfluxDbConfiguration(Configuration.GetSection("InfluxDb"));
+        public string ApiKey => Configuration.GetValue<string>("ApiKey");
+    }
+}
--- Sybon.Auth/Startup.cs
+++ Sybon.Auth/Startup.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using App.Metrics;
+using App.Metrics.Extensions.Configuration;
 using AutoMapper;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
 using Sybon.Archive.Client.Api;
 using Sybon.Archive.Client.Client;
 using Sybon.Auth.ApiStubs;
@@ -27,10 +30,15 @@ namespace Sybon.Auth
     [UsedImplicitly]
     public class Startup
     {
+        private const string ServiceName = "Sybon.Auth";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            SecurityConfiguration = new AuthSecurityConfiguration(configuration.GetSection("Security"));
         }
+
+        private AuthSecurityConfiguration SecurityConfiguration { get; }
 
         private IConfiguration Configuration { get; }
 
@@ -39,10 +47,34 @@ namespace Sybon.Auth
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-            var securityConfig = Configuration.GetSection("Security");
-            services.AddMvc();
 
-            services.AddSwagger("Sybon.Auth", "v1");
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                .Report.ToInfluxDb(options =>
+                {
+                    options.InfluxDb.Password = SecurityConfiguration.InfluxDb.Password;
+                    options.InfluxDb.UserName = SecurityConfiguration.InfluxDb.UserName;
+                    options.InfluxDb.BaseUri = new Uri(SecurityConfiguration.InfluxDb.Url);
+                    options.InfluxDb.Database = SecurityConfiguration.InfluxDb.Database;
+                    options.FlushInterval = TimeSpan.FromSeconds(1);
+                })
+                .Configuration.ReadFrom(Configuration)
+                .Configuration.Configure(
+                    options =>
+                    {
+                        options.AddAppTag(ServiceName);
+                        options.AddEnvTag("development");
+                    })
+                .Build();
+
+            services.AddMetrics(metrics);
+            services.AddMetricsReportScheduler();
+            services.AddMetricsTrackingMiddleware();
+            services.AddMetricsEndpoints();
+            
+            
+            services.AddMvc(options => options.AddMetricsResourceFilter());
+
+            services.AddSwagger(ServiceName, "v1");
             
             services.AddDbContext<AuthContext>(options =>
             {
@@ -67,10 +99,10 @@ namespace Sybon.Auth
             services.AddScoped<IPermissionsApi, PermissionsApi>();
             services.AddSingleton<IProblemsApi>(new ProblemsApi(new Configuration
             {
-                BasePath = securityConfig.GetValue<string>("Sybon.ArchiveUrl"),
+                BasePath = SecurityConfiguration.SybonArchive.Url,
                 ApiKey = new Dictionary<string, string>
                 {
-                    {"api_key", securityConfig.GetValue<string>("ApiKey")}
+                    {"api_key", SecurityConfiguration.ApiKey}
                 }
             }));
 
@@ -83,6 +115,8 @@ namespace Sybon.Auth
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors(builder => builder.AllowAnyOrigin());
+            app.UseMetricsAllMiddleware();
+            app.UseMetricsAllEndpoints();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
